aa5235f00ee7a675318e219854c8105887db0601
evandartois <89926363+evandartois@users.noreply.github.com>
2022-09-28 15:49:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
+#TP1_evan_dartois&charlie_
+
+
 #exo1
 def palindrome():
     def palindrome(mot):
-        lg = len(mot)
-        if lg == 1:
+        lg = len(mot) #on calcule la longueur du mot
+        if lg == 1: #si cette longueur est égale à 1, le mot est logiquement un palindrome
             return "Votre mot est un palindrome"
-        elif lg > 1 and mot[0] == mot[-1]:
-            return palindrome(mot[1:-1])
+        elif lg > 1 and mot[0] == mot[-1]: #si la longueur du mot est supérieur à 1 et que la dernière lettre et la première du mot sont pareils
+            return palindrome(mot[1:-1])  #par récursivité, on rappelle la fonction en enlevant la première et la dernière lettre
         else:
-            return "Votre mot n'est pas un palindrome"
+            return "Votre mot n'est pas un palindrome" #si la condition du elif n'est pas valide alors le mot n'est pas un palindrome
+        
+def lancement_palindrome():
+    mot = input("entrez un mot :")  #on demande un mot à l'utilisateur
+    print(palindrome(mot))  #affichage du return de la fonction palindrome
+#lancement_palindrome()   #exo numéro 1
+
+
 
 #exo2
 def crypter(ch):
-    ch_final = ''
-    ch = ch.upper()
-    for lettre in ch:
-        if ord(lettre) >= 65 and ord(lettre) < 90:
-            lettre = chr(ord(lettre)+1)
-            ch_final += lettre
-        elif ord(lettre) == 90:
-            lettre = 'A'
-            ch_final += lettre
+    ch_final = ''   #on crée une chaîne d'un ensemble vide 
+    ch = ch.upper()   #on met la chaine en majuscules
+    for lettre in ch:   #on prend lettre par lettre
+        if ord(lettre) >= 65 and ord(lettre) < 90:    #avec l'aide de la table  ASCII, on vérifie que le caractère est bien une majuscule
+            lettre = chr(ord(lettre)+1)  #si le contrôle est valide, on attribue à la lettre en question la lettre qui la suit 
+            ch_final += lettre  #on ajoute cette lettre dans la chaîne crée initialement
+        elif ord(lettre) == 90:  #si la lettre est un z
+            lettre = 'A'         #on lui donne comme valeur la lettre A
+            ch_final += lettre   #on ajoute par la suite à la chaîne final la letter A
         else:
-            print('Le message contient des caractères spéciaux: cryptage impossible')
+            print('Le message contient des caractères spéciaux: cryptage impossible')  #si la chaine entrée par l'utilisateur n'a pas que des lettres, message d'erreur
 
-    return ch_final
+    return ch_final  #on return la chaîne final cryptée
+
+def lancement_crypter():
+    chaine = input("ecrivez une chaîne de caractères :")  #on demande une chaîne à l'utilisateur
+    print(crypter(chaine))  #affichage du return de la fonction crypter
+#lancement_crypter()  #exo numéro2
 
 #exo3
 #1
 def nb_min(password):
-    nombre_minuscule = 0
+    nombre_minuscule = 0  #on initialise
     for carac in password:
         if ord(carac) >= 97 and ord(carac) <= 122:
             nombre_minuscule +=1
@@ -46,7 +61,7 @@ def nb_maj(password):
 def nb_alpha(password):
     nombre_non_alpha = 0
     for carac in password:
-        if ord(carac) <= 65 or (ord(carac) >= 90 and ord(carac) <= 97) or ord(carac) >= 122:
+        if ord(carac) < 65 or (ord(carac) > 90 and ord(carac) < 97) or ord(carac) > 122:
             nombre_non_alpha +=1
 
     return nombre_non_alpha
@@ -80,9 +95,7 @@ if __name__ == '__main__':
     mot = input("entrez un mot :")
     print(palindrome(mot))
 
-    chaine = input('Ecrivez un message ')
-    print(crypter((chaine)))
-    """
+        """
     ps = 'MAL-LDKHHIHIOD-JH'
     print(nb_min(ps))
     print(nb_maj(ps))
